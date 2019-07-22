f4aa75ce8263597514e028671509de759edf7c50
Blake Lamb <blake.lamb31@gmail.com>
2019-07-22 12:26:13 -0600

    created the basic server, added crtls and functionality from the server to create a new user and check if that username is already in use

--- recess/db/add_user.sql
+++ recess/db/add_user.sql
@@ -1,4 +1,4 @@
 INSERT INTO users(user_first_name, user_last_name, user_address, user_city, user_state, user_zip, user_phone,username, password)
-VALUES (${user_first_name}, ${user_last_name}, ${user_address}, ${user_city}, ${user_state}, ${user_zip}, ${user_phone}, ${username}, ${password});
+VALUES (${user_first_name}, ${user_last_name}, ${user_address}, ${user_city}, ${user_state}, ${user_zip}, ${user_phone}, ${username}, ${password})
 
 RETURNING *;
