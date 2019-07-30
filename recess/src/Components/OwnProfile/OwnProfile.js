d5ce11b5d67edffd79645a4a9f24eee13be8d785
Caleb McBride <atfbassman@yahoo.com>
2019-07-30 15:37:52 -0600

    ownProfile 75% done

--- recess/src/Components/OwnProfile/OwnProfile.js
+++ recess/src/Components/OwnProfile/OwnProfile.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
 import './OwnProfile.css'
+import {getUser, checkUser} from '../../Redux/reducers/users'
 
-function OwnProfile() {
-    return <div className='message-box'>
-      Hello World
-    </div>
+function OwnProfile(props) {
+  useEffect(() => {
+    props.checkUser()
+    props.getUser(props.match.params.user_id)
+  }, [])
+  console.log(props.user)
+    return (
+      
+      <div>
+      { props.user ? 
+        <div className='background'>
+          <div className="playerCard">
+            <img className="profilePic" src={props.user.data.user_img}/>
+            <h2>{props.user.data.username}</h2>
+            <h3>{`${props.user.data.user_city}, ${props.user.data.user_state}`}</h3>
+            <h3>Sports</h3>
+          </div>
+        </div>
+        : 
+        <div>YOHO</div>
+      }
+      </div>
+    )
   }
 
-  export default OwnProfile;
+  let mapStateToProps = state => {
+    console.log(state)
+    return {
+      user: state.users.data,
+      selected: state.users.selected
+
+    }
+  }
+
+  export default connect(mapStateToProps, {getUser, checkUser})(OwnProfile);
