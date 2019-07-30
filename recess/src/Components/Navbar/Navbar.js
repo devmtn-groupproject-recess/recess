1c35e8007afa86fc74ec589b93e57870c6e63da4
Blake Lamb <blake.lamb31@gmail.com>
2019-07-30 14:46:02 -0600

    Added code to change navbar to only render the navigation links when a user is logged in

--- recess/src/Components/Navbar/Navbar.js
+++ recess/src/Components/Navbar/Navbar.js
@@ -1,39 +1,60 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from "react-router-dom";
 import './Navbar.css'
-import {logout} from '../../Redux/reducers/users'
+import {logout, checkUser} from '../../Redux/reducers/users'
 import {connect} from 'react-redux'
 
 function Navbar(props) {
+
+  useEffect( () => {
+    props.checkUser()
+    console.log('Hit Navbar')
+  }, [])
 
   let handleLogout = () => {
     console.log("hit")
     props.logout()
   }
+
+  console.log(props.user)
+
     return(
-      <div>
-          <nav className="navbar">
-              <div>
-                <i className="logo">RECESS</i>
-                <label for="toggle" class="label">&#9776;</label>
-                <input type="checkbox" id="toggle"/>
-                <div class="menu">
-                  <Link to="/home" className="words">Home</Link>
-                  <Link to="/events" className="words">Events</Link>
-                  <Link to="/profile" className="words">Profile</Link>
-                  <p className="words" onClick={handleLogout}>Logout</p>
+      <div className="navbar">
+        <i className="logo">RECESS</i>
+        {props.user &&
+        <div>
+            <nav >
+                <div>
+                  <label for="toggle" class="label">&#9776;</label>
+                  <input type="checkbox" id="toggle"/>
+                  <div class="menu">
+                    <Link to="/home" className="words">Home</Link>
+                    <Link to="/events" className="words">Events</Link>
+                    <Link to={`/profile/${props.user.data.user_id}`} className="words">Profile</Link>
+                    <p className="words" onClick={handleLogout}>Logout</p>
+                  </div>
                 </div>
-              </div>
-          </nav>
-          <br/>
-          <br/>
-          <br/>
+            </nav>
+            
+            <br/>
+            <br/>
+            <br/>
+          </div>
+        }
       </div>  
     )
   }
 
   //Unsure where to put the link to the other profile at so we can discuss that and add that to the router.
   let mapDispatchToProps = {
-    logout
+    logout,
+    checkUser
   }
-  export default connect(null, mapDispatchToProps)(Navbar);
+
+  let mapStateToProps = state => {
+    console.log(state)
+    return {
+      user: state.users.data
+    }
+  }
+  export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
