4a960db2183ce5e3a0c257f9ca365c84881848b5
Caleb McBride <atfbassman@yahoo.com>
2019-07-30 16:56:21 -0600

    Edit profile starte

--- recess/src/Components/OwnProfile/OwnProfile.js
+++ recess/src/Components/OwnProfile/OwnProfile.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
 import './OwnProfile.css'
 import {getUser, checkUser} from '../../Redux/reducers/users'
 
@@ -16,13 +17,14 @@ function OwnProfile(props) {
         <div className='background'>
           <div className="playerCard">
             <img className="profilePic" src={props.user.data.user_img}/>
+            <h1>{`${props.user.data.user_first_name} ${props.user.data.user_last_name}`}</h1>
             <h2>{props.user.data.username}</h2>
             <h3>{`${props.user.data.user_city}, ${props.user.data.user_state}`}</h3>
             <h3>Sports</h3>
           </div>
         </div>
         : 
-        <div>YOHO</div>
+        <Redirect to='/'/>
       }
       </div>
     )
