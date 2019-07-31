f834fdc4b5dfa7d242e737f4fcb9e018f707aa56
Blake Lamb <blake.lamb31@gmail.com>
2019-07-31 10:26:01 -0600

    Finished Prfile View

--- recess/server/Controllers/events.js
+++ recess/server/Controllers/events.js
@@ -31,6 +31,26 @@ module.exports = {
             res.status(409).send(error)
         }
     },
+    getUserCreatedEvents: async (req, res) => {
+        try {
+                if(!req.session.user) {
+                    res.status(404).send("No user is logged in!")
+                }
+
+                const db = req.app.get('db')
+                const {user_id} = req.session.user
+                
+                const userCreatedEventsList = await db.get_user_created_events(user_id)
+
+                console.log(898989, userCreatedEventsList)
+        
+                res.status(200).send(userCreatedEventsList)
+            }
+        catch(error) {
+            console.log('Error in events Ctrl (getUserCreatedEvents)', error)
+            res.status(409).send(error)
+        }
+    },
     getSubscribedEvents: async (req, res) => {
         try{
             const db = req.app.get('db')
--- recess/src/Components/OwnProfile/OwnProfile.js
+++ recess/src/Components/OwnProfile/OwnProfile.js
@@ -3,11 +3,13 @@ import {connect} from 'react-redux'
 import {Redirect} from 'react-router-dom'
 import './OwnProfile.css'
 import {getUser, checkUser} from '../../Redux/reducers/users'
+import {getUserCreatedEvent} from '../../Redux/reducers/events'
 
 function OwnProfile(props) {
   useEffect(() => {
     props.checkUser()
     props.getUser(props.match.params.user_id)
+    props.getUserCreatedEvent()
   }, [])
   console.log(props.user)
     return (
@@ -15,7 +17,7 @@ function OwnProfile(props) {
       <div>
       { props.user ? 
         <div className='background'>
-          {props.selected && props.selected.user_img &&
+          {props.selected && 
             <div className="playerCard">
               <img className="profilePic" src={props.selected.user_img}/>
               <h1>{`${props.selected.user_first_name} ${props.selected.user_last_name}`}</h1>
@@ -23,9 +25,30 @@ function OwnProfile(props) {
               <h3>{`${props.selected.user_city}, ${props.selected.user_state}`}</h3>
               <h3>Sports</h3>
               <button onClick={() => props.history.push(`/profile/edit/${props.match.params.user_id}`)}>Edit</button>
+              {props.events && 
+                <div>
+                  <h1>Events I Created:</h1>
+                  {props.events.map( (singleEvent, index) => {
+                    let timeDate = new Date(singleEvent.event_date)
+                    let showTime = timeDate.toLocaleTimeString()
+                    let showDate = timeDate.toLocaleDateString()
+                  
+                      return(
+                        <div key={index} onClick={ () => props.history.push(`/events/${singleEvent.event_id}`)}>
+                          <h3>{`${singleEvent.event_name} - ${singleEvent.event_type}`}</h3>
+                          <p>{`${singleEvent.event_city}, ${singleEvent.event_state}`}</p>
+                          <p>{`${showDate} ${showTime}`}</p>
+                        </div>
+                      )
+                    })
+                  }
+                </div>
+    
+              }
             </div>
 
           }
+
         </div>
         : 
         <Redirect to='/'/>
@@ -38,9 +61,9 @@ function OwnProfile(props) {
     console.log(state)
     return {
       user: state.users.data,
-      selected: state.users.selected
-
+      selected: state.users.selected,
+      events: state.events.data
     }
   }
 
-  export default connect(mapStateToProps, {getUser, checkUser})(OwnProfile);
+  export default connect(mapStateToProps, {getUser, checkUser, getUserCreatedEvent})(OwnProfile);
